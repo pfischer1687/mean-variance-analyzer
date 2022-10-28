6a7deb6d3c61f24d95ae57578defe83c7e7742be
pfischer1687 <pfisch789@outlook.com>
2022-10-27 19:50:09 -0700

    Transforms data to use MDX, creates pages programatically, adds dynamic images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsby Site",
+    title: "My Super Cool Blog",
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog/`,
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
 };
