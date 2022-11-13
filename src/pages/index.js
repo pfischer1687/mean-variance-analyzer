bb5f6e21d990093921b7b0f34efe7ef0608a3a0e
pfischer1687 <pfisch789@outlook.com>
2022-11-12 22:03:25 -0800

    Adds 'start now' button to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,24 @@ import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
 import Seo from "../components/seo";
 import * as styles from "../components/index.module.css";
+import { Link } from "gatsby";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <div class={styles.indexGridContainer}>
-        <div class={styles.headerContainer}>
+      <div className={styles.indexGridContainer}>
+        <div className={styles.headerContainer}>
           <h1>Mean-Variance Analyzer</h1>
-          <h2>
-            This is a mean-variance analyzer for financial portfolios via Monte
-            Carlo simulation.
-          </h2>
+          <div>
+            <p>
+              This is a mean-variance analyzer for financial portfolios via
+              Monte Carlo simulation. Learn more by following the{" "}
+              <Link to="/tutorial">tutorial</Link>.
+            </p>
+            <button>
+              <Link to="/start">Start now</Link>
+            </button>
+          </div>
         </div>
         <StaticImage
           alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
