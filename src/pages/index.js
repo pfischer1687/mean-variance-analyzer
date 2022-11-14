8b355305e1cb2350b158c702fa12f7363728a008
pfischer1687 <pfisch789@outlook.com>
2022-11-13 23:42:36 -0800

    Animates the hero text subtitle and 'start now' button to fade in from the left staggered by half a second

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,24 +6,31 @@ import * as styles from "../components/index.module.css";
 import { Link } from "gatsby";
 
 const IndexPage = () => {
+  React.useEffect(() => {
+    const hiddenElements = document.querySelectorAll(`.${styles.hidden}`);
+    hiddenElements.forEach((e, index) => {
+      e.classList.add(`${styles.show}`);
+      e.style.transitionDelay = `${index * 500}ms`;
+    });
+  });
   return (
     <Layout pageTitle="Home Page">
       <div className={styles.indexGridContainer}>
         <StaticImage
           alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
           src="../images/clifford.jpg"
-          width={300}
+          className={styles.containerImg}
         />
         <div className={styles.headerContainer}>
           <h2 className={styles.heroTextTop}>Mean-Variance Analyzer</h2>
           <h2 className={styles.heroTextBottom}>Mean-Variance Analyzer</h2>
           <div>
-            <p>
+            <p className={styles.hidden}>
               This is a mean-variance analyzer for financial portfolios via
               Monte Carlo simulation. Learn more by following the{" "}
               <Link to="/tutorial">tutorial</Link>.
             </p>
-            <button>
+            <button className={styles.hidden}>
               <Link to="/start">Start now</Link>
             </button>
           </div>
