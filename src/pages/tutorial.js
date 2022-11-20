2fbfa62237692624230a6b0d8637b6a719e9229b
pfischer1687 <pfisch789@outlook.com>
2022-11-20 15:54:18 -0800

    Updates the introduction of the tutorial

--- src/pages/tutorial.js
+++ src/pages/tutorial.js
@@ -17,42 +17,41 @@ const TutorialPage = () => {
           help students interested in finance start their journey learning about
           the history of portfolio optimization. In 1952, economist Harry
           Markowitz published "Portfolio Selection" which introduced modern
-          portfolio theory (MPT), or mean-variance analysis
+          portfolio theory (MPT), or mean-variance analysis.
           <sup>
             <a href="#references">[1]</a>
-          </sup>
-          . The goal of MPT is to maximize the expected return of a portfolio
-          for a given level of risk, measured by the variance of its asset
-          prices. The optimized part of the risk-return spectrum is referred to
-          as the efficient frontier.
+          </sup>{" "}
+          The goal of MPT is to maximize the expected return of a portfolio for
+          a given level of risk - measured by the variance of its asset prices.
+          The optimized part of the risk-return spectrum is referred to as the
+          efficient frontier.
         </p>
         <p>
           In 1966, economist William Sharpe published "Mutual Fund Performance"
           which defined the reward-to-variability ratio (now known as the Sharpe
-          ratio)
+          ratio).
           <sup>
             <a href="#references">[2]</a>
-          </sup>
-          . This research grew from his part in the development of the capital
+          </sup>{" "}
+          This research grew from his part in the development of the capital
           asset pricing model (CAPM) for which he shared the 1990 Alfred Nobel
-          Memorial Prize in Economic Sciences with Markowitz and Merton Miller
+          Memorial Prize in Economic Sciences with Markowitz and Merton Miller.
           <sup>
             <a href="#references">[3]</a>
-          </sup>
-          . In 1994, he published "The Sharpe Ratio" which defined the ex ante
-          Sharpe ratio
+          </sup>{" "}
+          In 1994, he published "The Sharpe Ratio" which defined the ex ante
+          Sharpe ratio.
           <sup>
             <a href="#references">[4]</a>
-          </sup>
-          . To calculate this value, we first define the differential return d
-          as a function of the historical return R<sub>F</sub> on fund F and the
+          </sup>{" "}
+          To calculate this value, we first define the differential return d as
+          a function of the historical return R<sub>F</sub> on fund F and the
           historical return R<sub>B</sub> on a benchmark portfolio or security
           (often the annualized 3 month Treasury bill rate, which at the time of
-          this site's development was 3.72%
+          this site's development was 3.72%):
           <sup>
             <a href="#references">[5]</a>
           </sup>
-          ):
         </p>
         <div>
           <StaticImage
@@ -74,61 +73,102 @@ const TutorialPage = () => {
         </div>
         <p>
           This site contains a preloaded set of data based on {numAssets}{" "}
-          popular assets including stocks, ETFs, cryptocurrencies, and more. The
-          data contains the annualized average monthly return percentage for the
-          close prices for each asset over its max period, the variance of these
-          values, as well as the covariance with each other asset. When
-          calculating the Sharpe ratio, it is common to look at the monthly
-          close prices (cite). Since portfolio optimization is about
-          diversifying assets, the developer chose to calculate the covariances
-          only over periods where both assets had data, while hoping to capture
-          the assets average and variance over a larger period of time for
-          better accuracy. Please refer to the{" "}
+          popular assets including stocks, ETFs, cryptocurrencies, and more.
+          This data contains the annualized returns (in %) from the average
+          monthly close price for each asset over its max period, the variance
+          of these values, and their covariance with respect to each other
+          asset's values. When calculating the Sharpe ratio, it is common to use
+          the monthly close prices.
+          <sup>
+            <a href="#references">[6]</a>
+          </sup>{" "}
+          Since portfolio optimization is an extension of asset diversification,
+          the developer chose to calculate the covariances only over periods
+          where both assets had data, while separately still capturing each
+          asset's returns and variance over a larger period of time for better
+          accuracy (in his amateur opinion). Note that this may negatively
+          affect the accuracy of the results and we are not liable for the
+          accuracy of this data nor its resulting information as per the{" "}
+          <Link to="/terms">Terms of Service</Link>. Please refer to the{" "}
           <a href="https://github.com/pfischer1687/get-json-data-for-mva">
             source code
           </a>{" "}
-          for how the data was gathered for more details. Given this data, this
-          site uses Monte Carlo simulation to generate 500,000 randomly
-          generated allocations to a given set of assets in a portfolio. To get
-          more accurate results, the problem could be turned into a quadratic
-          programming problem via:
+          for how the data was gathered for more details.{" "}
+        </p>
+        <p>
+          This site uses Monte Carlo simulation to approximate the efficient
+          frontier from 500,000 random allocations to a given set of assets
+          (please see the site's{" "}
+          <a href="https://github.com/pfischer1687/mean-variance-analyzer">
+            source code
+          </a>{" "}
+          for more details). The ex ante Sharpe ratio is calculated via the
+          following formula, where mu is the vector of mean returns, Q is the
+          covariance matrix, and x are the weights (allocations) such that they
+          are all positive and add to 1 (or 100%):
           <sup>
-            <a href="#references">[6]</a>
+            <a href="#references">[7]</a>
           </sup>
         </p>
         <div>
           <StaticImage
-            className={styles.latexImageS}
+            className={styles.latexImageSCalc}
             src="../images/maximize-sharpe.png"
             alt="Equation for ex ante Sharpe ratio in terms of weights"
           />
         </div>
         <p>
-          , where mu is the vector of mean returns, Q is the covariance matrix,
-          and x are the weights such that they are all positive and add to 1 (or
-          100%). In our case, the maximum ex ante Sharpe ratio is calculated and
-          plotted on a graph of the annualized average historical returns of the
-          portfolio vs. the monthly historical standard deviation of the close
-          price (please see the <a href="#">source code</a> for more details).
-          The single asset data is plotted as well. The efficient fronteir is
+          In our case, the portfolio that produces the largest Sharpe ratio out
+          of the 500,000 is plotted on a graph of each portfolio's mean return
+          vs. its risk, or standard deviation. The efficient frontier is
           calculated by splitting the risk into 15 equally spaced bins and
-          finding the maximum return within each bin. 1,000 of the 500,000
-          randomly generated weighted portfolios are then chosen at random to be
-          plotted to outline the hyperbolic shape known as the Markowitz bullet.
-          The tangency portfolio draws a straight line from the risk-free rate
-          (or banchmark) return to the ex ante Sharpe ratio, representing the
-          line of best capital allocation when the risk-free investment is
-          incorporated into the portfolio, which in general extends past the ex
-          ante Sharpe ratio, but this part is hidden for the sake of being able
-          to better see the efficient frontier.
-        </p>
-        <p>
-          Keep in mind there are critics of the Sharpe ratio{" "}
+          choosing the largest return from within each bin. Note that the
+          efficient frontier here is approxmiated via Monte Carlo simulation and
+          a more accurate calculation can be done via quadratic programming -
+          which is beyond the scope of this tutorial but the reader is
+          encouraged to learn more about this after experimenting with the site.
           <sup>
             <a href="#references">[7]</a>
+          </sup>{" "}
+          1,000 of the 500,000 randomly generated portfolios are chosen at
+          random and plotted to outline a hyperbolic shape known as the
+          Markowitz bullet, which shows the general distribution of returns as a
+          function of different allocations. The tangency portfolio represents
+          the line of best capital allocation when the risk-free investment is
+          incorporated into the portfolio and can be visualized by drawing a
+          straight line from the risk-free rate (or benchmark) to the Sharpe
+          ratio. In general, the tangency portfolio extends past the Sharpe
+          ratio, but this part is hidden on the site to better see the efficient
+          frontier.
+        </p>
+        <p>
+          Note, on this site we only cover one of the earliest and most basic
+          formulas for portfolio optimization. The field has grown a lot since
+          then and the site is meant only for educational purposes to get the
+          reader started on their journey towards eventually learning about how
+          portfolio optimization is done in practice today (i.e. this site's
+          information can not be considered as financial advice or
+          recommendations for any investments as per the{" "}
+          <Link to="/terms">Terms of Service</Link>). Standard calculations of
+          the annualized ex ante Sharpe ratio have a wide range of criticisms,
+          such as how historical returns cannot guarantee an asset's future
+          performance, how it assumes a normal distribution of returns which in
+          practice may underestimate tail risk, how it does not take into
+          account serial correlation of the portfolio's assets which has been
+          shown in some cases to overestimate the calculation by over 65%
+          <sup>
+            <a href="#references">[8]</a>
           </sup>
-          . Make sure to also check out the Sortino, Treynor ratios and the
-          Black-Litterman model. And many more.
+          , and many more. The reader is encouraged to look into other portfolio
+          optimization strategies such as the Sortino ratio (which penalizes
+          downside volatility more than upside), the Treynor ratio (which
+          measures the risk by its beta, or how its volatility correlates with
+          the market's), the Black-Litterman model (which takes into account an
+          investor's beliefs about the future of an asset's returns), and many
+          more.
+          <sup>
+            <a href="#references">[9]</a>
+          </sup>
         </p>
         <h2>Tutorial</h2>
         <StaticImage src="../images/tutorial-home.png" alt="tmp" />
@@ -206,6 +246,13 @@ const TutorialPage = () => {
             , November 17, 2022.
           </li>
           <li>
+            Fernando, Jason. "Sharpe Ratio Formula and Definition with
+            Examples." Investopedia. June 06, 2022.{" "}
+            <a href="https://www.investopedia.com/terms/s/sharperatio.asp">
+              https://www.investopedia.com/terms/s/sharperatio.asp
+            </a>
+          </li>
+          <li>
             "Maximizing the Sharpe ratio." IEOR 4500.{" "}
             <a href="https://people.stat.sc.edu/sshen/events/backtesting/reference/maximizing%20the%20sharpe%20ratio.pdf">
               https://people.stat.sc.edu/sshen/events/backtesting/reference/maximizing%20the%20sharpe%20ratio.pdf
@@ -219,6 +266,13 @@ const TutorialPage = () => {
               doi:10.2469/faj.v58.n4.2453
             </a>
             .
+          </li>
+          <li>
+            Idzorek, Thomas M. "A step-by-step guide to the Black-Litterman
+            model." July 20, 2004.{" "}
+            <a href="https://people.duke.edu/~charvey/Teaching/BA453_2006/Idzorek_onBL.pdf">
+              https://people.duke.edu/~charvey/Teaching/BA453_2006/Idzorek_onBL.pdf
+            </a>
           </li>
         </ol>
       </div>
