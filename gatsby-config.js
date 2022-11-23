99487beddd846475dc4e202d1d66e6103fbfb27f
pfischer1687 <pfisch789@outlook.com>
2022-11-22 21:51:45 -0800

    Adds twitter card information to Seo component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,32 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
 module.exports = {
   siteMetadata: {
     title: "Mean-Variance Analyzer",
+    description: "Learn about modern portfolio theory - interactively!",
+    author: "@PaulFis43236408",
+    // siteUrl: "#",
   },
   plugins: [
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
     `gatsby-transformer-sharp`,
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Mean-Variance Analyzer`,
         short_name: `MVA`,
         start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
+        background_color: `#001220`,
+        theme_color: `#096002`,
         display: `standalone`,
         icon: "src/favicon.png",
       },
